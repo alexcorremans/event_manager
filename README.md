ff3cc21f1966f32df8e9d9b40dab7dac0296d450
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-01-02 21:29:05 +0100

    done!

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a tutorial I followed to become more familiar with the following:
+- file manipulation
+- reading content from a CSV file and transforming it into a standardized format
+- using data to contact a remote service (in this case, Google's Civic Information API through the Google API Client Gem)
+- populating a template with user data using ERB
+
+From The Odin Project's [curriculum](https://www.theodinproject.com/lessons/event-manager)
--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"O")[0..4]
@@ -10,23 +11,39 @@ def legislators_by_zipcode(zip)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
-                            address: zip,
-                            levels: 'country',
-                            roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislators_string = legislator_names.join(", ")
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
   rescue
-    legislators_string = "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
-  return legislators_string
 end
 
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts "EventManager initialized."
+
 contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new(template_letter)
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
-  puts "#{name} #{zipcode} #{legislators}"
+
+  form_letter = erb_template.result(binding)
+  save_thank_you_letters(id,form_letter)
 end
